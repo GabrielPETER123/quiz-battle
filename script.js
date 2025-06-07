2347f4f82bb96c87e861840e07d61b885fe4e5e2
GabrielPETER123 <gabriel.peter@ynov.com>
2025-06-07 03:21:46 +0200

    presque fini le joueur solo

--- script.js
+++ script.js
@@ -1,79 +1,171 @@
+const PlayerContainer = document.getElementById('player-container');
+const TwoPlayersContainer = document.getElementById('two-players-container');
+const PlayerNames = document.getElementById('player-names');
+const PlayerOptions = document.getElementById('player-options');
+const playerNameInput = document.getElementById('player-name');
+const player1NameInput = document.getElementById('player1-name');
+const player2NameInput = document.getElementById('player2-name');
+const PlayersPoints = document.getElementById('players-points');
+const PlayerTurn = document.getElementById('player-turn');
+const StartQuizzButton = document.getElementById('start-quizz');
+const QuizzContainer = document.getElementById('quizz-container');
+const HomeScreen = document.getElementById('home-screen');
+const EndScreen = document.getElementById('end-screen');
+const Result = document.getElementById('result');
+const Winner = document.getElementById('winner');
+const Question = document.getElementById('question');
+const Answer1 = document.getElementById('answer1');
+const Answer2 = document.getElementById('answer2');
+const Answer3 = document.getElementById('answer3');
+const Answer4 = document.getElementById('answer4');
+
+let question = '', questions = {};
+let playerName = '', player1Name = '', player2Name = '';
+let pcPoint = 0, playerPoint = 0, player1Point = 0, player2Point = 0;
+let playerTurn = '';
+
 const ChooseNumberOfPlayer = () => {
-
-    const startButton =  document.getElementById('start-button');
-    startButton.style.display = 'none';
-
-    const choosePlayer = document.createElement('div');
-    choosePlayer.classList.add('choose-player');
-    choosePlayer.innerHTML = `
-        <h2>Choisissez le nombre de joueur</h2>
-        <div class="player-options">
-            <button onclick="StartGame(1);" class="player-option" id="player-option1">1 Joueur</button>
-            <button onclick="StartGame(2);" class="player-option" id="player-option2">2 Joueurs</button>
-        </div>
-    `;
-    document.body.appendChild(choosePlayer);
+    HomeScreen.style.display = 'none';
+    PlayerOptions.style.display = 'block';
 }
 
-const StartGame = (nbr) => {
-    
-    const choosePlayer = document.getElementById('start-button');
-    choosePlayer.style.display = 'none';
+const PreQuizz = async (nbr) => {
+    PlayerOptions.style.display = 'none';
+    PlayerNames.style.display = 'block';
+    //* Affiche les choix 1 ou 2 joueurs */
+    if (nbr === 0) {
+        PlayerContainer.style.display = 'block';
+    }
     if (nbr === 1) {
-        QuizzOnePlayer();
-    }
-
-    if (nbr === 2) {
-        QuizzTwoPlayers();
+        TwoPlayersContainer.style.display = 'block';
     }
 }
 
-const QuizzOnePlayer = async () => {
+const StartQuizz = async () => {
+    //* Cache les input de nom de joueur */
+    PlayerNames.style.display = 'none';
 
-    const quizz = document.createElement('div');
-    quizz.innerHTML = `
-        <h2>Quizz contre le PC</h2>
-        <div class="quizz-container">
-            <p class="question" id="question"></p>
-            <div class="answers">
-                <button class="answer" id="answer1"></button>
-                <button class="answer" id="answer2"></button>
-                <button class="answer" id="answer3"></button>
-                <button class="answer" id="answer4"></button>
-            </div>
-        </div>
-    `;
-	
-    let questions = await GetQuestions();
-	
-	const indexQuestion = 0
-	if (questions.questions.length > 1) {
-		indexQuestion = Math.random(0, questions.questions.length - 1);	
-	}
-    const question = questions.questions[indexQuestion];
-    delete questions.questions[indexQuestion]
-	
-    quizz.getElementsByClassName('question')[0].innerText = question.question;
-    
-    quizz.getElementById('answer1').innerText = question.answers[0];
-    quizz.getElementById('answer2').innerText = question.answers[1];
-    quizz.getElementById('answer3').innerText = question.answers[2];
-    quizz.getElementById('answer4').innerText = question.answers[3];
+    //* Affiche le bouton de démarrage du quizz */
+    QuizzContainer.style.display = 'block';
 
-	document.body.appendChild(quizz);
+    //* Prend les questions et Affiche la prochaine question */
+    questions = await GetQuestions();
+    NextQuestion();
 }
-
-
-const QuizzTwoPlayers = () => {
-
-
-}
-
 
 const GetQuestions = async () => {
-    const response = await fetch('questions.json');
-    const questions = await response.json().then(data => {
+    const Response = await fetch('questions.json');
+    const Questions = await Response.json().then(data => {
 		return data;
 	});
-	return questions;
+	return Questions;
+}
+
+const NextQuestion = async () => {
+    Result.innerText = '';
+
+	let indexQuestion = 0;
+	if (questions.questions.length > 1) {
+		indexQuestion = Math.floor(Math.random() * questions.questions.length);
+	}else if (questions.questions.length === 1) {
+        indexQuestion = 0;
+    }else{
+        QuizzContainer.style.display = 'none';
+        EndScreen.style.display = 'block';
+        //! C'est horrible mais ça prend qu'une ligne
+        Winner.innerText = (playerTurn === 'PC' || playerTurn === playerName) ?((pcPoint > playerPoint) ? 'PC' : ((pcPoint === playerPoint) ? 'Égalité': playerName)): ((player1Point > player2Point) ? player1Name : ((player1Point === player2Point) ? 'Égalité' : player2Name));
+        return;
+    };
+
+    question = questions.questions[indexQuestion];
+    questions.questions.splice(indexQuestion, 1);
+
+    Question.innerText = question.question;
+    Answer1.innerText = question.answers[0].answer;
+    Answer2.innerText = question.answers[1].answer;
+    Answer3.innerText = question.answers[2].answer;
+    Answer4.innerText = question.answers[3].answer;
+
+    if(playerTurn === 'PC') {
+        //** Désactive les bouttons */
+        Answer1.disabled = true;
+        Answer2.disabled = true;
+        Answer3.disabled = true;
+        Answer4.disabled = true;
+        
+        setTimeout(() => {
+            let answer = Math.floor(Math.random() * question.answers.length);
+            CheckAnswer(answer);
+        }, 1000);
+        
+        //** Réactive les bouttons */
+        Answer1.disabled = false;
+        Answer2.disabled = false;
+        Answer3.disabled = false;
+        Answer4.disabled = false;
+    }
+
+}
+
+const CheckAnswer = (answer) => {
+    let isCorrect = false;
+
+    if (question.answers[answer].isCorrect) {
+        Result.innerText = 'Correct!';
+        isCorrect = true;
+    } else {
+        Result.innerText = 'Incorrect!';
+        isCorrect = false;
+    }
+
+    if (isCorrect) {
+        switch (playerTurn) {
+            case playerName :
+                playerPoint++;
+                break;    
+            case player1Name :
+                player1Point++;
+                break;
+            case player2Name :
+                player2Point++;
+                break;
+            default:
+                pcPoint++;
+                break;
+        }
+    }
+    WaitForNextQuestion();
+}
+
+const WaitForNextQuestion = () => {
+    setTimeout(() => {
+        if (playerTurn === playerName || playerTurn === 'PC') {
+            playerTurn = (playerTurn === 'PC') ? playerName : 'PC';
+            PlayersPoints.innerText = playerName + ' : ' + playerPoint + '\nPC : ' + pcPoint;
+            PlayerTurn.innerText = 'Au tour de ' + playerTurn + ' !';
+        } else {
+            
+        }
+        NextQuestion();
+    }, 2000);
+}
+
+const GetPlayerName = (nbr) => {
+    //* Récupèration de nom selon 1 ou 2 joueurs */
+    if (nbr === 0) {
+        playerName = playerNameInput.value;
+        PlayersPoints.innerText = playerName + ' : ' + playerPoint + '\nPC : ' + pcPoint;
+        playerTurn = playerName;
+        PlayerTurn.innerText = 'Au tour de ' + playerTurn + ' !';
+        PlayerContainer.style.display = 'none';
+    }else{
+        player1Name, player2Name = player1NameInput.value, player2NameInput.value;
+        PlayersPoints.innerText = player1Name + ' : ' + player1Point + '\n' + player2Name + ' : ' + player2Point;
+        playerTurn = player1Name;
+        PlayerTurn.innerText = 'Au tour de ' + playerTurn + ' !';
+        TwoPlayersContainer.style.display = 'none';
+    }
+
+    //* Affiche le bouton de démarrage du quizz */
+    StartQuizzButton.style.display = 'block';
 }
